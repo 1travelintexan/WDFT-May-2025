77136142f24025489a56113bef09171142a7e255
Joshua George <horheyinc8@gmail.com>
2025-06-25 16:36:54 +0200

    images

--- module-3/week-1/day-5/full-stack-project/backend/models/Pet.model.js
+++ module-3/week-1/day-5/full-stack-project/backend/models/Pet.model.js
@@ -10,6 +10,8 @@ const petSchema = new Schema({
   },
   image: {
     type: String,
+    default:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/65761296352685.5eac4787a4720.jpg",
   },
   owner: {
     type: Schema.Types.ObjectId,
--- module-3/week-1/day-5/full-stack-project/backend/routes/pet.routes.js
+++ module-3/week-1/day-5/full-stack-project/backend/routes/pet.routes.js
@@ -2,29 +2,36 @@ const { isAuthenticated } = require("../middlewares/jwt.middleware");
 const PetModel = require("../models/Pet.model");
 const UserModel = require("../models/User.model");
 const router = require("express").Router();
-
+const uploader = require("../middlewares/cloudinary.config");
 //route to create a pet, then add that pet to the array inside a user
-router.post("/add-pet", isAuthenticated, async (req, res) => {
-  const theOwnerId = req.payload._id;
-  try {
-    const createdPet = await PetModel.create({
-      ...req.body,
-      owner: theOwnerId,
-    });
-    const updatedUser = await UserModel.findByIdAndUpdate(
-      theOwnerId,
-      {
-        $push: { pets: createdPet._id },
-      },
-      { new: true }
-    );
+router.post(
+  "/add-pet",
+  isAuthenticated,
+  uploader.single("imageUrl"),
+  async (req, res) => {
+    const theOwnerId = req.payload._id;
+    try {
+      console.log("here is the new file", req.file);
+      const createdPet = await PetModel.create({
+        ...req.body,
+        owner: theOwnerId,
+        image: req.file?.path,
+      });
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        theOwnerId,
+        {
+          $push: { pets: createdPet._id },
+        },
+        { new: true }
+      );
 
-    res.status(201).json({ createdPet, updatedUser });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
+      res.status(201).json({ createdPet, updatedUser });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
   }
-});
+);
 //route to get all pets for one specific user
 router.get("/user-pets", isAuthenticated, async (req, res) => {
   try {
